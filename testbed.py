e7a109a5c78963e45c9a0bed7f3ee65315dd13a8
dkozak06 <dkozak.06@gmail.com>
2017-02-22 18:09:17 -0700

    Finished algorithm, roughly

--- testbed.py
+++ testbed.py
@@ -24,7 +24,7 @@ def x2p(x=np.array([]), perplexity=40):
         it = 1
 
         if i % 100 == 0:
-            print ("Computing conditional probabilities for point ", i, " of ", n, "...")
+            print("Computing conditional probabilities for point ", i, " of ", n, "...")
         p = np.exp(-np.square(tri[i]) / (2 * sigma[i])) / (np.sum(np.exp(-np.square(tri[i]) / (2 * sigma[i]))))
         entropy = -sum(np.multiply(p[p != 0], np.log2(p[p != 0])))
 
@@ -67,10 +67,47 @@ def tsne(x=np.array([]), intrinsic_dim = 2, perplexity = 30):
     momentum_final = .8
     eta = 500
     min_gain = 0.01
-    Y = np.random.randn(n, intrinsic_dim)
+    y = np.random.randn(n, intrinsic_dim)
     dY = np.zeros((n, intrinsic_dim))
-    iY = np.zeros((n, intrinsic_dim))
+    dYold = np.zeros((n, intrinsic_dim))
     P = x2p(x, perplexity)
+    P = np.divide(P + np.transpose(P), 2*n)
+    P = np.maximum(P, 1e-12)
+    Q = np.zeros((n, n))
+
+    for iter in range(maxit):
+        # Compute Q
+
+        distance = sp.spatial.distance.pdist(y, metric='euclidean')
+        tri = np.zeros((n, n))
+        tri[np.triu_indices(n, 1)] = distance
+        tri[np.tril_indices(n, -1)] = distance
+        np.fill_diagonal(tri, np.infty)
+        for j in range(n):
+            q = np.exp(-np.square(tri[j]))/np.sum(np.exp(-np.square(tri[j])))
+            Q[j] = q
+
+        # Compute gradient
+        PQ = P-Q
+        for i in range(n):
+            dY = np.multiply(2, np.sum(np.multiply(PQ[i]-np.transpose(PQ)[i], y[i,:]-y)))
+
+        # Set new outputs
+        if iter < 20:
+            momentum = momentum_init
+        else:
+            momentum = momentum_final
+
+        y += eta * dY + momentum * (dY - dYold)
+        dYold = dY
+
+        # Compute Current Cost
+        C = np.sum(np.multiply(P, np.log(np.divide(P,Q))))
+        print("Iteration", (iter + 1), ": Error is ", round(C, 2))
+
+        if iter == 100:
+            P = P/4
+    return y
 
 if __name__ == "__main__":
     print ("Run Y = tsne.tsne(X, no_dims, perplexity) to perform t-SNE on your dataset.")
